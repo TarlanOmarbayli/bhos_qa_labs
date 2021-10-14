4f87372b76df7b179b8d5f964878fc5e352a1c82
Tarlan Omarbayli <tarlanomarbayli@gmail.com>
2021-10-14 13:43:06 +0400

    initial commit

---
+++ src/test/java/ClassroomTester.java
@@ -0,0 +1,33 @@
+import my_classroom.Classroom;
+import java.util.*;
+import java.lang.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ClassroomTester {
+    Classroom infosec = new Classroom(2018,"Infosec",25,24,240,"Khayyam Masiyev");
+
+    @Test
+    public void test1(){
+        assertEquals(2018, infosec.getEntry_year());
+    }
+    @Test
+    public void test2(){
+        assertEquals(240, infosec.getMinimumTotalCredits());
+    }
+    @Test
+    public void test3(){
+        assertEquals("Infosec 2018", infosec.getName_of_classroom());
+    }
+    @Test
+    public void test4(){
+        assertEquals(2023, infosec.getGraduationYear());
+    }
+    @Test
+    public void test5(){
+        assertEquals("Khayyam Masiyev", infosec.getName_of_curator());
+    }
+
+
+}
